2973f4605abb5db545a734be0000f32c44510c3a
Shaquille Michael <smichael@MacBook-Pro-2.local>
2018-01-15 15:31:00 -0600

    Adding whats needed for swagger api viewer

--- src/main/java/com/pm/app/apartment/controller/MainController.java
+++ src/main/java/com/pm/app/apartment/controller/MainController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
 public class MainController {
 
-	@RequestMapping(value = "/v1/{locationID}",method = RequestMethod.GET)
+	@RequestMapping(value = "getContact/{locationID}",method = RequestMethod.GET)
 	public String getContactInfo(@PathVariable String locationID) {
         return locationID;
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.PUT)
 	public String custom() {
 		return "custom";
 	}
