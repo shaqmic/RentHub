d9bab8fc9e7e24ff1ae2d26f9c082ad35baf42c6
Shaquille Michael <smichael@MacBook-Pro-2.local>
2018-01-15 11:22:43 -0600

    First Commit, Setting up project and adding main controller

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	
+	public void getContactInfo(String locationID) {
+		
+	}
+}
